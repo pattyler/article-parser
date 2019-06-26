4d9a962c46b89c27ba5a7160908652e380c56c05
Patrick Tyler <ptyler2608@gmail.com>
2019-06-26 20:32:28 +0900

    Minor updated to SQL query. Added ORDER BY clause

--- src/main/java/io/github/patfromthe90s/util/SQLQueries.java
+++ src/main/java/io/github/patfromthe90s/util/SQLQueries.java
@@ -10,6 +10,6 @@ public class SQLQueries {
 	public static final String GET_LAST_UPDATED = "SELECT lastUpdated FROM site WHERE siteUrl = ?;";
 	public static final String UPDATE_LAST_UPDATED = "UPDATE site SET lastUpdated = ? WHERE siteUrl = ?;";
 	
-	public static final String GET_ARTICLE = "SELECT url, data, date FROM article WHERE datetime(date) BETWEEN datetime(?) AND datetime(?)";
+	public static final String GET_ARTICLE = "SELECT url, data, date FROM article WHERE datetime(date) BETWEEN datetime(?) AND datetime(?) ORDER BY datetime(date) ASC";
 	public static final String INSERT_ARTICLE = "INSERT INTO article (url, data, date) VALUES (?, ?, ?);";
 }
