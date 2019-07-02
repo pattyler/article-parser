ddae99eb30e1a4c742a7a856a51f0c287df6791a
Patrick Tyler <ptyler2608@gmail.com>
2019-07-02 12:53:41 +0900

    Added main program runner sample

--- src/main/java/io/github/patfromthe90s/viewer/ArticleViewer.java
+++ src/main/java/io/github/patfromthe90s/viewer/ArticleViewer.java
@@ -3,11 +3,15 @@ package io.github.patfromthe90s.viewer;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.github.patfromthe90s.exception.DaoServiceException;
 import io.github.patfromthe90s.service.DaoService;
 
 public class ArticleViewer {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ArticleViewer.class);
 	private final DaoService daoService;
 	
 	public ArticleViewer(DaoService daoService) {
@@ -27,7 +31,7 @@ public class ArticleViewer {
 						
 					});
 		} catch (DaoServiceException e) {
-			e.printStackTrace();
+			LOGGER.error(e.getMessage(), e);
 		}
 	}
 
